11b33f016acce41501c270d0709cdac4b459e45b
Alex Marleron <alexmarleron@gmail.com>
2023-03-20 20:18:41 +0300

    Initial Commit

---
+++ QuickNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickNotes
+//
+//  Created by san on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
